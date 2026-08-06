b4087e03fc8567e6881cdc426709c326d0106df3
syncfusion <buildautomation@syncfusion.com>
2026-08-06 07:24:12 +0000

    34.2.2

---
+++ pdfviewer/Views/FreeText.xaml.cs
@@ -0,0 +1,30 @@
+﻿using syncfusion.demoscommon.wpf;
+
+namespace syncfusion.pdfviewerdemos.wpf
+{
+    /// <summary>
+    /// Interaction logic for FreeText.xaml
+    /// </summary>
+    public partial class FreeText : DemoControl
+    {
+        public FreeText()
+        {
+            InitializeComponent();
+        }
+        public FreeText(string themename) : base(themename)
+        {
+            InitializeComponent();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            pdfviewer1.Unload(true);
+            pdfviewer1 = null;
+            if (this.DataContext is FreeTextViewModel)
+            {
+                (this.DataContext as FreeTextViewModel).DocumentStream.Dispose();
+                this.DataContext = null;
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ pdfviewer/Views/SmartRedaction.xaml.cs
@@ -0,0 +1,32 @@
+﻿using syncfusion.demoscommon.wpf;
+
+namespace syncfusion.pdfviewerdemos.wpf
+{
+    /// <summary>
+    /// Interaction logic for SmartRedaction.xaml
+    /// </summary>
+    public partial class SmartRedaction : DemoControl
+    {
+        #region Constructor
+        public SmartRedaction()
+        {
+            InitializeComponent();
+        }
+        #endregion
+        public SmartRedaction(string themename) : base(themename)
+        {
+            InitializeComponent();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            pdfviewer1.Unload(true);
+            pdfviewer1 = null;
+            if (this.DataContext is SmartRedactionViewModel)
+            {
+                (this.DataContext as SmartRedactionViewModel).DocumentStream.Dispose();
+                this.DataContext = null;
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
