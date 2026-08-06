309c175ed8b6cf6b473508e7e33902afa60f2473
syncfusion <buildautomation@syncfusion.com>
2026-08-06 07:19:38 +0000

    34.2.2

---
+++ pdfviewer/ViewModel/BookmarkNavigationViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Resources;
+
+namespace syncfusion.pdfviewerdemos.wpf
+{
+    /// <summary>
+    /// Represents the view model class
+    /// </summary>
+    public class BookmarkNavigationViewModel 
+    {
+        private Stream m_documentStream;
+        /// <summary>
+        /// Gets or sets the documnet path.
+        /// </summary>
+        public Stream DocumentStream
+        {
+            get
+            {
+                return m_documentStream;
+            }
+            set
+            {
+                m_documentStream = value;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BookmarkNavigationViewModel"/> class.
+        /// </summary>
+        public BookmarkNavigationViewModel()
+        {
+            m_documentStream = GetFileStream("PDF_Succinctly.pdf");
+        }
+        private Stream GetFileStream(string fileName)
+        {
+            Uri uriResource = new Uri("/syncfusion.pdfviewerdemos.wpf;component/Assets/" + fileName, UriKind.Relative);
+            StreamResourceInfo streamResourceInfo = Application.GetResourceStream(uriResource);
+            return streamResourceInfo.Stream;
+        }
+    }
+}
---
+++ pdfviewer/ViewModel/RedactionViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Resources;
+
+namespace syncfusion.pdfviewerdemos.wpf
+{
+    /// <summary>
+    /// Represents the view model class
+    /// </summary>
+    public class RedactionViewModel 
+    {
+        private Stream m_documentStream;
+
+        /// <summary>
+        /// Gets or sets the documnet path.
+        /// </summary>
+        public Stream DocumentStream
+        {
+            get
+            {
+                return m_documentStream;
+            }
+            set
+            {
+                m_documentStream = value;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RedactionViewModel"/> class.
+        /// </summary>
+        public RedactionViewModel()
+        {
+            m_documentStream = GetFileStream("Redacted.pdf");
+        }
+        private Stream GetFileStream(string fileName)
+        {
+            Uri uriResource = new Uri("/syncfusion.pdfviewerdemos.wpf;component/Assets/" + fileName, UriKind.Relative);
+            StreamResourceInfo streamResourceInfo = Application.GetResourceStream(uriResource);
+            return streamResourceInfo.Stream;
+        }
+    }
+}
